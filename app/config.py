2df9b9347e09532effbefaa64745c8efdad2147f
Shandene Gray <shandenegray20@gmail.com>
2021-02-21 09:31:44 -0500

    your commit message

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
-
 class Config(object):
-    """Base Config Object"""
-    DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+ """Base Config Object"""
+ DEBUG = False
+ SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+ 
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
